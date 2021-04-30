04a2e3af84a39b9091f17668409fe6ecbbe1b4d3
diskit <diskit.1980@gmail.com>
2021-05-01 08:24:30 +0900

    vite-appで作られるものに合わせta

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,25 @@
 <script lang='ts'>
-	import Header from './components/Header.svelte';
-	const a = 'this string contains some <strong>HTML!!!</strong>';
+	import HelloWorld from './components/HelloWorld.svelte';
 	const src = 'logo.svg';
-	let selected = false;
 </script>
   
 <style>
 
-:global(*) {
-	box-sizing: border-box;
-}
-
 :global(body) {
-	margin: 0;
-	font-family: Arial, Helvetica, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 
 img {
-	width: 10vw;
-}
-
-.selected {
-	color: #f00;
+  width: 200px;
+  height: 200px;
 }
 
 </style>
-  
-<Header a={"ass"}></Header>
-<div class="App">{@html a}</div>
+
 <img {src} alt="logo">
-<div class:selected>test</div>
-<button on:click={() => selected = !selected}>click</button>
+<HelloWorld msg="Hello Svelte"></HelloWorld>
---
+++ src/components/HelloWorld.svelte
@@ -0,0 +1,12 @@
+<script lang='ts'>
+	export let msg = '';
+  let count = 0;
+</script>
+  
+<style>
+
+</style>
+  
+<h1>{msg}</h1>
+<button on:click={() => count++}>count is: {count}</button>
+<p>Edit ...</p>
