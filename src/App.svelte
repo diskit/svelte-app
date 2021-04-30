b360fce108217d99ba90c34ef5e7675a744bb208
diskit <diskit.1980@gmail.com>
2021-04-30 18:29:21 +0900

    add components

--- src/App.svelte
+++ src/App.svelte
@@ -1,70 +1,33 @@
-<script lang='typescript'>
-	import {onMount} from 'svelte';
-	let count: number = 0;
-	onMount(() => {
-	  const interval = setInterval(() => count++, 1000);
-	  return () => {
-		clearInterval(interval);
-	  };
-	});
-  </script>
+<script lang='ts'>
+	import Header from './components/Header.svelte';
+	const a = 'this string contains some <strong>HTML!!!</strong>';
+	const src = 'logo.svg';
+	let selected = false;
+</script>
   
-  <style>
-	:global(body) {
-	  margin: 0;
-	  font-family: Arial, Helvetica, sans-serif;
-	}
-	.App {
-	  text-align: center;
-	}
-	.App code {
-	  background: #0002;
-	  padding: 4px 8px;
-	  border-radius: 4px;
-	}
-	.App p {
-	  margin: 0.4rem;
-	}
+<style>
+
+:global(*) {
+	box-sizing: border-box;
+}
+
+:global(body) {
+	margin: 0;
+	font-family: Arial, Helvetica, sans-serif;
+}
+
+img {
+	width: 10vw;
+}
+
+.selected {
+	color: #f00;
+}
+
+</style>
   
-	.App-header {
-	  background-color: #f9f6f6;
-	  color: #333;
-	  min-height: 100vh;
-	  display: flex;
-	  flex-direction: column;
-	  align-items: center;
-	  justify-content: center;
-	  font-size: calc(10px + 2vmin);
-	}
-	.App-link {
-	  color: #ff3e00;
-	}
-	.App-logo {
-	  height: 36vmin;
-	  pointer-events: none;
-	  margin-bottom: 3rem;
-	  animation: App-logo-spin infinite 1.6s ease-in-out alternate;
-	}
-	@keyframes App-logo-spin {
-	  from {
-		transform: scale(1);
-	  }
-	  to {
-		transform: scale(1.06);
-	  }
-	}
-  </style>
-  
-  <div class="App">
-	<header class="App-header">
-	  <img src="/logo.svg" class="App-logo" alt="logo" />
-	  <p>Edit <code>src/App.svelte</code> and save to reload.</p>
-	  <p>Page has been open for <code>{count}</code> seconds.</p>
-	  <p>
-		<a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
-		  Learn Svelte
-		</a>
-	  </p>
-	</header>
-  </div>
-  
+<Header a={"ass"}></Header>
+<div class="App">{@html a}</div>
+<img {src} alt="logo">
+<div class:selected>test</div>
+<button on:click={() => selected = !selected}>click</button>
---
+++ src/components/Header.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+	export let a = '';
+
+</script>
+  
+<style>
+header {
+  display: flex;
+  height: 64px;
+  padding: 8px;
+  align-items: center;
+  background-color: #ff0;
+}
+</style>
+  
+<header>header {a}</header>  
