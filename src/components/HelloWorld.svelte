29370b0cfc12956d1f53762fb33a9797f27309fb
diskit <diskit.1980@gmail.com>
2021-05-01 09:48:49 +0900

    apply callback function

--- src/components/HelloWorld.svelte
+++ src/components/HelloWorld.svelte
@@ -1,6 +1,10 @@
 <script lang='ts'>
 	export let msg = '';
   let count = 0;
+
+  const increment = () => {
+    count++;
+  }
 </script>
   
 <style>
@@ -8,5 +12,5 @@
 </style>
   
 <h1>{msg}</h1>
-<button on:click={() => count++}>count is: {count}</button>
+<button on:click={increment}>count is: {count}</button>
 <p>Edit ...</p>
